5eaaae5c060329c2e27ed33d4c50a36d0e1daaaa
KairigoShawn <kairigonjogu@outlook.com>
2021-09-05 01:39:06 +0300

    adds basic js functionallity

---
+++ akan.js
@@ -0,0 +1,37 @@
+const getakanname = function (e) {
+   const femaleNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ];
+  const maleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+
+  const birthday = document.getElementById("selected-date").value;
+  if (!birthday) {
+    return alert("Please Select Birthday");
+  }
+  const day = new Date(birthday).getDay();
+  const femaleGender = document.getElementById("female-radio-btn").checked;
+  const maleGender = document.getElementById("male-radio-btn").checked;
+  if (femaleGender) {
+    alert("Your Akan Name is " + femaleNames[day]);
+  } else if (maleGender) {
+    alert("Your Akan Name is " + maleNames[day]);
+  } else {
+    return alert("Please select Gender");
+  }
+};
+
+document.querySelector("#akan-form").addEventListener("submit", getakanname);
